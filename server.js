b66201521c98976e75b4b3308fe1e73f17ab5a03
ditmemayahah <ghetvietcode@gmail.com>
2025-08-24 21:58:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ let apiResponseData = {
 let lastPrediction = null; 
 const fullHistory = []; 
 
-const WEBSOCKET_URL = "wss://websocket.azhkthg1.net/websocket?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbW91bnQiOjAsInVzZXJuYW1lIjoiU0NfYXBpc3Vud2luMTIzIn0.hgrRbSV6vnBwJMg9ZFtbx3rRu9mX_hZMZ_m5gMNhkw0";
+const WEBSOCKET_URL = "wss://websocket.azhkthg1.net/websocket?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbW91bnQiOjAsInVzZXJuYW1lIjoiU0NfYXBpc3Vud2luMTIzIn0.hgrRbSV6vnBwJMg9ZFtbx3rRu9m_hZMZ_m5gMNhkw0";
 const WS_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
     "Origin": "https://play.sun.win"
@@ -89,6 +89,8 @@ function connectWebSocket() {
                     if (lastPrediction === result) {
                         predictor.correctPredictions++;
                         correctnessStatus = "ĐÚNG";
+                    } else {
+                        correctnessStatus = "SAI";
                     }
                     predictor.totalPredictions++;
                 }
